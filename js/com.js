d073e1ef22f9e7ee9aeb2c2de49c7d160bd3aa30
unknown <windows@.(none)>
2014-07-30 22:37:47 +0200

    Connect to COM-port

--- js/com.js
+++ js/com.js
@@ -1,28 +1,30 @@
-// 
-// var serialPort = require('serialport');
-// 
-// 
-// // serialPort.list(function (err, ports) {
-  // // ports.forEach(function(port) {
-    // // console.log(port.comName, port.pnpId, port.manufacturer);
-  // // });
-// // });
-// 
-// var sp = new serialPort.SerialPort("COM8", {
-  // baudrate: 9600,
-  // dataBits: 8, 
-  // parity: 'odd', 
-  // stopBits: 1,
-  // parser: serialPort.parsers.readline("\n"),
-  // flowControl: true
-// }, false);
-// 
-// 
-// sp.open(function () {
-  // console.log('open');
-  // sp.on('data', function(data) {
-    // console.log('>>> ' +data);
-  // });
-  // sp.write('status\r');
-// });
-// 
+var serialPort = require('serialport');
+var comPort;
+var sp;
+
+serialPort.list(function (err, ports) {
+  ports.forEach(function(port) {
+  	if (port.pnpId == "USB\\VID_2047&PID_082F&MI_01\\6&20B65F81&0&0001") {
+
+  		sp = new serialPort.SerialPort(port.comName, {
+		  baudrate: 9600,
+		  dataBits: 8, 
+		  parity: 'odd', 
+		  stopBits: 1,
+		  parser: serialPort.parsers.readline("\n"),
+		  flowControl: true
+		}, false);
+
+		sp.open(function () {
+		  console.log('open on port: ' + port.comName);
+		  sp.on('data', function(data) {
+		    console.log('>>> ' +data);
+		  });
+		  sp.write('status\r');
+		});	
+  	}
+  });
+  if (!sp) {
+  	console.log('No device connected!');
+  }
+});
