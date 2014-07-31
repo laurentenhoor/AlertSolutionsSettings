2e0ee1b825644207ad78d59565dd792c042a8ea7
unknown <windows@.(none)>
2014-07-31 03:12:20 +0200

    Com port end of day

--- js/com.js
+++ js/com.js
@@ -2,30 +2,44 @@ var serialPort = require('serialport');
 var comPort;
 var sp;
 
-var results = [];
+var results = {};
+var subresults = [];
+var nextCmd = '';
+var receivingCmd = '';
+var isReceiving = false;
 
 serialPort.list(function (err, ports) {
   ports.forEach(function(port) {
   	
-  	console.log(port.comName);
-  	console.log(port.pnpId);
+  	// console.log(port.comName);
+  	// console.log(port.pnpId);
   	
   	if (port.pnpId.split('\\')[1] == "VID_2047&PID_082F&MI_01") {
 
   		sp = new serialPort.SerialPort(port.comName, {
 		  baudrate: 9600,
 		  dataBits: 8, 
-		  parity: 'odd', 
-		  stopBits: 1,
-		  parser: serialPort.parsers.readline("\n"),
-		  flowControl: true
+		  
+		  stopBits: 1
 		}, false);
 
 		sp.open(function () {
 		  console.log('open on port: ' + port.comName);
 		  sp.on('data', function(data) {
 		    console.log('>>> ' +data);
-		    results.push(data);
+		    
+		    // if (isReceiving) {
+		    	// subresults.push(data);
+		    // } else if (data.indexOf(nextCmd)) {
+		    	// console.log('isReceiving!!');
+		    	// receivingCmd = nextCmd;
+		    	// isReceiving = true;
+		    // } else if (data == '') {
+		    	// console.log('end of receiving')
+		    	// isReceiving = false;
+		    	// results[receivingCmd] = subresults;
+		    // }
+		    
 		  });
 		  sp.write('ver?\r');
 		});	
@@ -38,3 +52,11 @@ serialPort.list(function (err, ports) {
 
 });
 
+
+
+function getData(cmd) {
+	
+	nextCmd = cmd;
+	sp.write(nextCmd+'\r');
+	
+};
