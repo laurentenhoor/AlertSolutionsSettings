e04165640b645e59daa7660096e3a2865d1f1b87
unknown <windows@.(none)>
2014-08-01 15:12:36 +0200

    Finished Settings Model

--- js/settings.js
+++ js/settings.js
@@ -1,10 +1,47 @@
+var SENSORS = {
+	'TL15' : {
+		name : 'Tilt Logger 15deg',
+		defaults : []
+	},
+	'TL90' : {
+		name : 'Tilt Logger 90deg',
+		defaults : []
+	},
+	'PIEZ' : {
+		name : 'Tilt Logger Piezometer',
+		defaults : []
+	},
+	'CUSTOM' : {
+		name : 'Custom Sensor',
+		defaults : []
+	}
+};
+
+var PULS_SENSORS = [SENSORS['TL15'], SENSORS['TL90'], SENSORS['PIEZ'], SENSORS['CUSTOM']];
+var VOLT_SENSORS = [SENSORS['CUSTOM']];
+var BARO_SENSORS = [SENSORS['CUSTOM']];
+var BATT_SENSORS = [SENSORS['CUSTOM']];
+
+
 var CHANNEL_TYPES = {
-	'0-10V' : {
-		defaults : {},
+	'voltage' : {
+		name : '0-10 V',
+		default_sensors : VOLT_SENSORS,
 		hide_fields : {}
 	},
 	'pulse' : {
-		defaults : {},
+		name : 'Pulse',
+		default_sensors : PULS_SENSORS,
+		hide_fields : {}
+	},
+	'baro' : {
+		name : 'Barometer',
+		default_sensors : BARO_SENSORS,
+		hide_fields : {}
+	},
+	'batt' : {
+		name : 'Battery',
+		default_sensors : BARO_SENSORS,
 		hide_fields : {}
 	}
 };
@@ -13,12 +50,18 @@ var DEVICES = {
 	'modemlogger' : {
 		channels : [{
 			id : 1,
-			channel_type : CHANNEL_TYPES['0-10V']
+			channel_type : CHANNEL_TYPES['voltage']
 		}, {
 			id : 2,
 			channel_type : CHANNEL_TYPES['pulse']
+		}, {
+			id : 3,
+			channel_type : CHANNEL_TYPES['baro']
+		}, {
+			id : 5,
+			channel_type : CHANNEL_TYPES['batt']
 		}]
 	}
 };
 
-console.log(DEVICES.modemlogger);
+console.log(DEVICES);
